855ae2eb25f3c1c88c8b88b34fd7f3e5d9a30f08
Andrew Chi <achi@cs.unc.edu>
2018-04-12 16:13:14 -0400

    [generic] add neural net classifier that sort of works
    Works with 3000 training examples and 3000 testing examples:
    $ head -n6000 randdata.txt | nice ./learn-length.py

--- tests/learn-length.py
+++ tests/learn-length.py
@@ -4,6 +4,10 @@ from sklearn import tree
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
 import sys
+import os
+import time
+
+TARGET_LEN = 500
 
 # >>> from sklearn import tree
 # >>> X = [[0, 0], [1, 1]]
@@ -11,29 +15,38 @@ import sys
 # >>> clf = tree.DecisionTreeClassifier()
 # >>> clf = clf.fit(X, Y)
 
-def pad_or_truncate(some_list, target_len):
-    return some_list[:target_len] + [0]*(target_len - len(some_list))
-
-TARGET_LEN = 500
+def pad_or_truncate(some_list, target_len, rand_pad=False):
+    if len(some_list) >= target_len:
+        return some_list[:target_len]
+    else:
+        pad_len = target_len - len(some_list)
+        new_list = some_list[:]
+        if rand_pad:
+            new_list.extend(os.urandom(pad_len))
+        else:
+            new_list.extend([0] * pad_len)
+        return new_list
 
 # read input
+t0 = time.clock()
 raw_data = []
 raw_lengths = []
 for line in sys.stdin:
     fields = line.strip().strip("[").strip("]").split("][")
     intfields = [int(x,16) for x in fields]
     raw_lengths.append(len(intfields))
-    raw_data.append(pad_or_truncate(intfields, TARGET_LEN))
+    raw_data.append(pad_or_truncate(intfields, TARGET_LEN, rand_pad=True))
 
 X = raw_data
 Y = raw_lengths
+t1 = time.clock()
 
-print("Loaded %d samples ranging from %d-%d bytes" %
-      (len(raw_data), min(raw_lengths), max(raw_lengths)))
+print("Loaded %d samples ranging from %d-%d bytes in %f seconds" %
+      (len(raw_data), min(raw_lengths), max(raw_lengths), t1-t0))
 
-clf = tree.DecisionTreeClassifier()
-scores = cross_val_score(clf, X, Y, cv=5)
-print("Decision Tree Classifier scores:", scores)
+# clf = tree.DecisionTreeClassifier()
+# scores = cross_val_score(clf, X, Y, cv=5)
+# print("Decision Tree Classifier scores:", scores)
 
 # clf.fit(X,Y)
 # print "Done"
@@ -47,6 +60,152 @@ print("Decision Tree Classifier scores:", scores)
 #         status = "WRONG"
 #     print(Y[i], Yprime[i], status)
 
-adaboost_clf = AdaBoostClassifier(n_estimators=10)
-scores = cross_val_score(adaboost_clf, X, Y, cv=5)
-print("AdaBoost Classifier scores:", scores)
+# Try out AdaBoostClassifier
+# adaboost_clf = AdaBoostClassifier(n_estimators=10)
+# scores = cross_val_score(adaboost_clf, X, Y, cv=5)
+# print("AdaBoost Classifier scores:", scores)
+
+#############################################################################
+
+# Try out neural network
+print("Trying out a Neural Network!")
+import numpy as np
+
+"""
+PyTorch: nn
+-----------
+
+A fully-connected ReLU network with one hidden layer, trained to predict y from x
+by minimizing squared Euclidean distance.
+
+This implementation uses the nn package from PyTorch to build the network.
+PyTorch autograd makes it easy to define computational graphs and take gradients,
+but raw autograd can be a bit too low-level for defining complex neural networks;
+this is where the nn package can help. The nn package defines a set of Modules,
+which you can think of as a neural network layer that has produces output from
+input and may have some trainable weights.
+"""
+import torch
+from torch.autograd import Variable
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 3000, TARGET_LEN, 30, 1
+
+# Create random Tensors to hold inputs and outputs, and wrap them in Variables.
+# x = Variable(torch.randn(N, D_in))
+# y = Variable(torch.randn(N, D_out), requires_grad=False)
+
+# Create numpy arrays that hold normalized inputs and outputs, and wrap them in
+# Variables.
+x = Variable(torch.FloatTensor(X[:N]) / 255.0)
+y = np.array(Y[:N], dtype=np.float32)
+y.reshape((-1,1))
+y = y / TARGET_LEN
+y = Variable(torch.from_numpy(y), requires_grad=False)
+
+# Use the nn package to define our model as a sequence of layers. nn.Sequential
+# is a Module which contains other Modules, and applies them in sequence to
+# produce its output. Each Linear Module computes output from input using a
+# linear function, and holds internal Variables for its weight and bias.
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, 10),
+    torch.nn.ReLU(),
+    torch.nn.Linear(10, D_out),
+)
+
+optimizer = torch.optim.SGD(model.parameters(), lr = 1e-5, momentum=0.9,
+                            weight_decay=1e-2)
+
+# The nn package also contains definitions of popular loss functions; in this
+# case we will use Mean Squared Error (MSE) as our loss function.
+loss_fn = torch.nn.MSELoss(size_average=False)
+
+#learning_rate = 1e-5
+for t in range(3000000): #35000 steps??
+    optimizer.zero_grad()
+
+    # Forward pass: compute predicted y by passing x to the model. Module objects
+    # override the __call__ operator so you can call them like functions. When
+    # doing so you pass a Variable of input data to the Module and it produces
+    # a Variable of output data.
+    y_pred = model(x)
+
+    # Compute and print loss. We pass Variables containing the predicted and true
+    # values of y, and the loss function returns a Variable containing the
+    # loss.
+    loss = loss_fn(y_pred, y)
+    print("SGDIteration:", t, "\tMeanSquaredError =", loss.data[0])
+
+    # Zero the gradients before running the backward pass.
+    #model.zero_grad()
+
+    # Backward pass: compute gradient of the loss with respect to all the learnable
+    # parameters of the model. Internally, the parameters of each Module are stored
+    # in Variables with requires_grad=True, so this call will compute gradients for
+    # all learnable parameters in the model.
+    loss.backward()
+
+    # Update the weights using gradient descent. Each parameter is a Variable, so
+    # we can access its data and gradients like we did before.
+    #for param in model.parameters():
+    #    param.data -= learning_rate * param.grad.data
+    optimizer.step()
+
+# Test on training results:
+print("-"*70)
+print("Training Accuracy")
+print("-"*70)
+y_pred_denormalized = torch.round(y_pred.data * float(TARGET_LEN)).numpy()
+y_denormalized = torch.round(y.data * float(TARGET_LEN)).view((-1,1)).numpy()
+y_correct = (y_pred_denormalized == y_denormalized)
+print("shape of y_pred:", y_pred_denormalized.shape)
+print("shape of y:", y_denormalized.shape)
+print("shape of y_correct:", y_correct.shape)
+print(np.hstack((y_pred_denormalized, y_denormalized, y_correct)))
+train_correct = int(np.sum(y_correct))
+train_total = y_correct.shape[0]
+print("%d correct out of %d (%2f %%)" %
+      (train_correct, train_total, float(train_correct)/train_total*100.0))
+
+# Test accuracy (only if there is data to test against)
+if (len(X) <= N):
+    sys.exit(0)
+
+print("-"*70)
+print("Testing Accuracy")
+print("-"*70)
+x = Variable(torch.FloatTensor(X[N:]) / 255.0, requires_grad=False)
+y = np.array(Y[N:], dtype=np.float32)
+y.reshape((-1,1))
+y = y / TARGET_LEN
+y = Variable(torch.from_numpy(y), requires_grad=False)
+
+y_pred = model(x)
+
+y_pred_denormalized = torch.round(y_pred.data * float(TARGET_LEN)).numpy()
+y_denormalized = torch.round(y.data * float(TARGET_LEN)).view((-1,1)).numpy()
+y_correct = (y_pred_denormalized == y_denormalized)
+print("shape of y_pred:", y_pred_denormalized.shape)
+print("shape of y:", y_denormalized.shape)
+print("shape of y_correct:", y_correct.shape)
+print(np.hstack((y_pred_denormalized, y_denormalized, y_correct)))
+test_correct = int(np.sum(y_correct))
+test_total = y_correct.shape[0]
+print("%d correct out of %d (%2f %%)" %
+      (test_correct, test_total, float(test_correct)/test_total*100.0))
+
+
+
+# Test on training:
+# for i, x_vec in enumerate(X):
+#     y_prediction = round(model(torch.FloatTensor(x_vec) / 255.0) * TARGET_LEN)
+#     y_actual = Y[i]
+#     if y_prediction == y_actual:
+#         result = "CORRECT"
+#     else:
+#         result = "WRONG"
+#     print("item #%d: predicted = %d, actual = %d, %s" \
+#           % (i, y_prediction, y_actual, result))
